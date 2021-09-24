3fba42ca3a64edfd9896f7101f91ac22e2c91029
IamChetang <chetang568568@gmail.com>
2021-09-24 12:13:42 +0530

    final script

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,34 @@
 'use strict';
+const input = document.getElementById('input');
+const btnTranslate = document.getElementById('btn-translate');
+const output = document.getElementById('class');
+const changeLang = document.getElementById('lang');
+
+// Functions
+
+const translateText = async function () {
+  const inputText = input.value;
+  const lang = changeLang.value;
+  if (inputText === '' || inputText === null) {
+    alert('Input text is mandatory');
+  } else {
+    const url = `https://api.funtranslations.com/translate/${lang}.json?text=${inputText}`;
+    const data = await fetch(url);
+    const result = await data.json();
+    output.innerHTML = result.contents.translated;
+  }
+};
+const translateLang = function () {
+  translateText();
+};
+
+const checkKey = function (e) {
+  if (e.key === 'Enter') {
+    translateText();
+  }
+};
+
+// Event listeners
+btnTranslate.addEventListener('click', translateText);
+document.addEventListener('keydown', checkKey);
+changeLang.addEventListener('change', translateLang);
